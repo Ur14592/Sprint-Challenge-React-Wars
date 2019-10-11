6af492fe9203aa9030b69df5ea3a7b8dab201b46
Alex Vega <alexvega880@Alexs-MacBook-Air.local>
2019-10-11 10:16:25 -0700

    getting things to display

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,33 +1,65 @@
-import React {useState, useEffect} from 'react';
+import React, { useState, useEffect } from "react";
 import './App.css';
-import SWPeople from './components/SWPeople';
 import axios from "axios";
+import SWPList from './components/SWPList';
+import styled from 'styled-components';
 
 
-const App = props => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+const App = props => {
 
 const [data, setData] = useState([]);
 useEffect(() => {
 axios 
-.get (`https://swapi.co/api/people/`)
-.then(response => {
-  setData(response.data.results);
-})
-.catch(error => {
-  console.log("ERROR beep boop", error);
-});
+  .get (`https://swapi.co/api/people/`)
+  .then(response => {
+    const data = response.data.results;
+    console.log("Character Data", data);
+    setData(data);
+  })
+  .catch(error => {
+    console.log("ERROR beep boop", error);
+  });
 },[]);
+
+
+
+
+
+const Container = styled.div`
+  color: #000000;
+  
+`;
+const Div = styled.div`
+
+`;
+
 
 
   return (
     <div className="App">
       <h1 className="Header">React Wars</h1>
+      <Container>
+        <Div>
+          {data.map(data => {
+            return (
+              <SWPList
+              key={data.id}
+              name={data.name}
+              height={data.height}
+              gender={data.gender}
+              skin={data['skin_color']}
+              eyes={data['eye_color']}
+              />
+            );
+          })}
+        </Div>
+      </Container>
     </div>
   );
 }
--- starwars/src/components/SWPList.js
+++ starwars/src/components/SWPList.js
@@ -1,2 +1,37 @@
-import React {useState, useEffect} from 'react';
-import SWPeople from './SWPeople';
+import React from 'react';
+import styled from 'styled-components';
+
+const Name = styled.h2`
+  color: #3B3B64;
+  
+`;
+const Height = styled.p`
+  color: #3B3B64;
+  
+`;
+const Gender = styled.p`
+  color: #3B3B64;
+  
+`;
+const Skin = styled.p`
+  color: #3B3B64;
+  
+`;
+const Eyes = styled.p`
+  color: #3B3B64;
+  
+`;
+
+const SWPList = props => {
+    return (
+        <div className="Key " key={props.key}>
+            <Name>{props.name}</Name>
+            <Height>{props.height}</Height>
+            <Gender> {props.gender}</Gender>
+            <Skin>{props.skin}</Skin>
+            <Eyes>{props.eyes}</Eyes>
+        </div>
+      );
+}
+
+export default SWPList;
