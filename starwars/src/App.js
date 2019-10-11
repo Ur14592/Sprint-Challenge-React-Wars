e17351ed03e0a30f76609480a2c5c124843a3acb
Alex Vega <alexvega880@Alexs-MacBook-Air.local>
2019-10-11 09:06:02 -0700

    starting on mvp

--- starwars/src/App.js
+++ starwars/src/App.js
@@ -1,13 +1,29 @@
-import React from 'react';
+import React {useState, useEffect} from 'react';
 import './App.css';
+import SWPeople from './components/SWPeople';
+import axios from "axios";
 
-const App = () => {
+
+const App = props => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
 
   // Fetch characters from the star wars api in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+
+const [data, setData] = useState([]);
+useEffect(() => {
+axios 
+.get (`https://swapi.co/api/people/`)
+.then(response => {
+  setData(response.data.results);
+})
+.catch(error => {
+  console.log("ERROR beep boop", error);
+});
+},[]);
+
 
   return (
     <div className="App">
